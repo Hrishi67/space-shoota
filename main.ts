e4076ac58277bcbc702a8337170329007b262d8a
Hrishi67 <vitalqb3@gmail.com>
2025-11-03 12:41:15 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
-
+let mainPlayer = sprites.create(img`
+    . f f f . . . . . . . . f f f . 
+    f f c . . . . . . . f c b b c . 
+    f c c . . . . . . f c b b c . . 
+    c f . . . . . . . f b c c c . . 
+    c f f . . . . . f f b b c c . . 
+    f f f c c . c c f b c b b c . . 
+    f f f c c c c c f b c c b c . . 
+    . f c 3 c c 3 b c b c c c . . . 
+    . c b 3 b c 3 b b c c c c . . . 
+    c c b b b b b b b b c c . . . . 
+    c b 1 b b b 1 b b b b f c . . . 
+    f b b b b b b b b b b f c c . . 
+    f b c b b b c b b b b f . . . . 
+    . f 1 f f f 1 b b b c f . . . . 
+    . . f b b b b b b c f . . . . . 
+    . . . f f f f f f f . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mainPlayer, 100, 100)
